bb08407c36c82b88ee3291637f3da316f6a565bd
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-01-22 15:53:08 +0100

    bst

---
+++ binary-search-tree/BSTTests/BSTTests.swift
@@ -0,0 +1,194 @@
+//
+//  BSTTests.swift
+//  BSTTests
+//
+//  Created by Vadim Dez on 22/01/2017.
+//  Copyright © 2017 Vadim Dez. All rights reserved.
+//
+
+import XCTest
+@testable import BinarySearchTree
+
+class BSTTests: XCTestCase {
+    var tree: BST?
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        self.tree = BST()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testNotNil() -> Void {
+        XCTAssertNotNil(self.tree)
+    }
+    
+    func testInsert() -> Void {
+        let value = 1;
+        self.tree?.insert(value: value);
+        
+        XCTAssertEqual(self.tree?.root?.value, value)
+    }
+    
+    func testInsertMultiple() -> Void {
+        self.tree?.insert(value: 10);
+        self.tree?.insert(value: 20);
+        self.tree?.insert(value: 25);
+        self.tree?.insert(value: 24);
+        self.tree?.insert(value: 5);
+        self.tree?.insert(value: 7);
+        self.tree?.insert(value: 9);
+        
+        XCTAssertEqual(self.tree?.root?.value, 10)
+        XCTAssertEqual(self.tree?.root?.right?.value, 20)
+        XCTAssertEqual(self.tree?.root?.right?.right?.value, 25)
+        XCTAssertEqual(self.tree?.root?.right?.right?.left?.value, 24)
+        XCTAssertEqual(self.tree?.root?.left?.value, 5)
+        XCTAssertEqual(self.tree?.root?.left?.right?.value, 7)
+        XCTAssertEqual(self.tree?.root?.left?.right?.right?.value, 9)
+    }
+    
+    func testInsertToTheLeft() -> Void {
+        let rootValue = 100
+        let leftValue = 80
+        self.tree?.insert(value: rootValue)
+        self.tree?.insert(value: leftValue)
+        
+        XCTAssertEqual(self.tree?.root?.value, rootValue)
+        XCTAssertEqual(self.tree?.root?.left?.value, leftValue)
+    }
+    
+    func testInsertToTheRight() -> Void {
+        let rootValue = 80
+        let rightValue = 100
+        
+        self.tree?.insert(value: rootValue)
+        self.tree?.insert(value: rightValue)
+        
+        XCTAssertEqual(self.tree?.root?.value, rootValue)
+        XCTAssertEqual(self.tree?.root?.right?.value, rightValue)
+    }
+    
+    func testInsertTwiceToTheRight() -> Void {
+        let rootValue = 80
+        let rightValue = 100
+        
+        self.tree?.insert(value: rootValue)
+        self.tree?.insert(value: rightValue)
+        self.tree?.insert(value: rightValue)
+        
+        XCTAssertEqual(self.tree?.root?.value, rootValue)
+        XCTAssertEqual(self.tree?.root?.right?.value, rightValue)
+        XCTAssertEqual(self.tree?.root?.right?.right?.value, rightValue)
+    }
+    
+    func testGetNodeCountOfEmptyTree() -> Void {
+        XCTAssertEqual(0, self.tree?.getNodeCount())
+    }
+    
+    func testGetNodeCount() -> Void {
+        XCTAssertEqual(0, self.tree?.getNodeCount())
+
+        self.tree?.insert(value: 100)
+        XCTAssertEqual(1, self.tree?.getNodeCount())
+
+        self.tree?.insert(value: 110)
+        XCTAssertEqual(2, self.tree?.getNodeCount())
+        
+        self.tree?.insert(value: 90)
+        XCTAssertEqual(3, self.tree?.getNodeCount())
+        
+        self.tree?.insert(value: 120)
+        self.tree?.insert(value: 130)
+        self.tree?.insert(value: 50)
+        self.tree?.insert(value: 600)
+        XCTAssertEqual(7, self.tree?.getNodeCount())
+    }
+    
+    func testPrintValues() -> Void {
+        XCTAssertEqual("", self.tree?.printValues())
+        
+        self.tree?.insert(value: 10)
+        XCTAssertEqual("10", self.tree?.printValues())
+        
+        self.tree?.insert(value: 11)
+        self.tree?.insert(value: 9)
+        self.tree?.insert(value: 8)
+        self.tree?.insert(value: 15)
+        let str = self.tree?.printValues()
+        
+        XCTAssertEqual("8 9 10 11 15", str)
+    }
+    
+    func testDeleteTree() -> Void {
+        self.tree?.deleteTree()
+        
+        XCTAssertNil(self.tree?.root)
+    }
+    
+    func testEmptyIsInTree() -> Void {
+        XCTAssertFalse(self.tree!.isInTree(value: 8))
+    }
+    
+    func testIsInTree() -> Void {
+        self.tree?.insert(value: 8)
+        XCTAssertTrue(self.tree!.isInTree(value: 8))
+        
+        self.tree?.insert(value: 5)
+        self.tree?.insert(value: 100)
+        self.tree?.insert(value: 20)
+        self.tree?.insert(value: 25)
+        self.tree?.insert(value: 3)
+        self.tree?.insert(value: 12)
+        self.tree?.insert(value: 0)
+        self.tree?.insert(value: -100)
+        XCTAssertTrue(self.tree!.isInTree(value: 20))
+        XCTAssertFalse(self.tree!.isInTree(value: 21))
+    }
+    
+    func testGetMin() -> Void {
+        self.tree?.insert(value: 10)
+        self.tree?.insert(value: 12)
+        self.tree?.insert(value: 8)
+        self.tree?.insert(value: 1)
+        self.tree?.insert(value: 6)
+        
+        XCTAssertEqual(try! self.tree?.getMin(), 1)
+        
+        self.tree?.insert(value: -10)
+        XCTAssertEqual(try! self.tree?.getMin(), -10)
+    }
+    
+    func testGetMax() -> Void {
+        
+        self.tree?.insert(value: 10)
+        self.tree?.insert(value: 12)
+        self.tree?.insert(value: 8)
+        self.tree?.insert(value: 1)
+        self.tree?.insert(value: 6)
+        
+        XCTAssertEqual(try! self.tree?.getMax(), 12)
+        
+        self.tree?.insert(value: 20)
+        self.tree?.insert(value: 19)
+        self.tree?.insert(value: 18)
+        XCTAssertEqual(try! self.tree?.getMax(), 20)
+    }
+}
--- binary-search-tree/binary-search-tree/BST.swift
+++ binary-search-tree/binary-search-tree/BST.swift
@@ -8,16 +8,125 @@
 
 import Foundation
 
+enum BSTError: Error {
+    case EmptyTree
+}
+
 class BST {
     var root: Node?
     
     func insert(value: Int) -> Void {
-        var i = self.root
+        let node = Node(value: value)
         
-        while i != nil {
-            i = i?.right
+        self.root = self.insertNode(root: self.root, node: node)
+    }
+    
+    private func insertNode(root: Node?, node: Node) -> Node {
+        if root == nil {
+            return node;
         }
         
-        i = Node(value: value)
+        if node.value >= (root?.value)! {
+            root?.right = self.insertNode(root: root?.right, node: node)
+        } else {
+            root?.left = self.insertNode(root: root?.left, node: node)
+        }
+        
+        return root!
+    }
+    
+    func getNodeCount() -> Int {
+        return self.getNodeCount(root: self.root)
+    }
+    
+    private func getNodeCount(root: Node?) -> Int {
+        if root == nil {
+            return 0
+        }
+        
+        return 1 + self.getNodeCount(root: root!.left) + self.getNodeCount(root: root!.right)
+    }
+    
+    func printValues() -> String {
+        return self.printValues(root: self.root)
+    }
+    
+    private func printValues(root: Node?) -> String {
+        var str: String = ""
+        
+        if root == nil {
+            return str
+        }
+        
+        let left: String = self.printValues(root: root!.left)
+        let right: String = self.printValues(root: root!.right)
+        
+        if left != "" {
+            str = "\( left ) "
+        }
+        
+        str += "\( root!.value )"
+        
+        if right != "" {
+            str += " \( right )"
+        }
+        
+        return str
+    }
+    
+    func deleteTree() -> Void {
+        self.root = nil
+    }
+    
+    func isInTree(value: Int) -> Bool {
+        return self.isInTree(node: self.root, value: value)
+    }
+    
+    private func isInTree(node: Node?, value: Int) -> Bool {
+        if node == nil {
+            return false
+        }
+        
+        if value == node?.value {
+            return true
+        }
+        
+        if value > node!.value {
+            return self.isInTree(node: node?.right, value: value)
+        }
+        
+        return self.isInTree(node: node?.left, value: value)
+    }
+    
+    func getMin() throws -> Int {
+        if self.root == nil {
+            throw BSTError.EmptyTree
+        }
+        
+        return self.getMin(node: self.root!)
+    }
+    
+    private func getMin(node: Node) -> Int {
+        if node.left == nil {
+            return node.value
+        }
+        
+        return self.getMin(node: node.left!)
+    }
+    
+    func getMax() throws -> Int {
+        if self.root == nil {
+            throw BSTError.EmptyTree
+        }
+        
+        return self.getMax(node: self.root!)
+    }
+    
+    private func getMax(node: Node) -> Int {
+        if node.right == nil {
+            return node.value
+        }
+        
+        return self.getMax(node: node.right!)
     }
 }
